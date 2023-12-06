a1b9fa6b71541a2a48ad29505059469a2a3e0434
Remag29 <73289897+Remag29@users.noreply.github.com>
2023-12-06 09:50:55 +0100

    Add testing for day 05 of 2023 part2

--- 2023/05/part2.py
+++ 2023/05/part2.py
@@ -1,4 +1,4 @@
-from part1 import *
+from part1 import load_data, get_tables
 
 if __name__ == '__main__':
     # Load data
@@ -11,27 +11,49 @@ if __name__ == '__main__':
     # Get seeds
     seeds = tables[0][0].split(' ')[1:]
     seeds = [int(seed) for seed in seeds]
-    print('Get a total of ' + str(len(seeds)) + ' seeds')
+    print(seeds)
+
+    mappings = []
+    for line in lines[2:]:
+        line = line.strip()
+        if line.endswith(":"):
+            mappings.append([])
+        elif len(line) > 0:
+            mappings[-1].append([int(i) for i in line.split(" ")])
+    print(mappings)
 
     # Get the final position
-    min_seed_pos = -1
-    # Get seeds two by two
-    for i in range(0, len(seeds), 2):
-        init_seed_pos = seeds[i]
-        step = seeds[i + 1]
-        print('Seed part ' + str(i / 2) + '/' + str(len(seeds) / 2))
-
-        for seed in range(init_seed_pos, init_seed_pos + step + 1):
-            actual_pos = seed
-
-            # Get the final position for the seed after each table
-            for table in tables[1:]:
-                actual_pos = get_new_position(actual_pos, table)
-
-            # Test if the final position is the lowest
-            if min_seed_pos == -1 or actual_pos < min_seed_pos:
-                min_seed_pos = actual_pos
+    res = 2 ** 64
+    for s, o in zip(seeds[::2], seeds[1::2]):
+        ranges = [(s, s + o - 1)]
+        for typemappings in mappings:
+            newranges = []
+            for l, h in ranges:
+                found = False
+                for md, ms, mo in typemappings:
+                    if l >= ms and h < ms + mo:
+                        newranges.append((l - ms + md, h - ms + md))
+                        found = True
+                    elif l < ms <= h < ms + mo:
+                        ranges.append((l, ms - 1))
+                        newranges.append((md, md + h - ms))
+                        found = True
+                    elif h >= ms + mo > l >= ms:
+                        ranges.append((ms + mo, h))
+                        newranges.append((md + l - ms, md + mo - 1))
+                        found = True
+                    elif l < ms and h >= ms + mo:
+                        ranges.append((l, ms - 1))
+                        newranges.append((md, md + mo - 1))
+                        ranges.append((ms + mo, h))
+                        found = True
+                    if found == True:
+                        break
+                if not found:
+                    newranges.append((l, h))
+            ranges = newranges.copy()
+        res = min(res, min(ranges)[0])
 
     # Get the lowest seed position
     print('-------------------')
-    print('Lowest seed position : ' + str(min_seed_pos))
+    print('Lowest seed position : ' + str(res))
